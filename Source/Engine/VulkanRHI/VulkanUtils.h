3f9360cc65161a37e6994cbfc74acb79cc6077f0
Mateusz Kołodziejczyk <mateusz643@gmail.com>
2022-07-20 18:36:07 +0200

    -add utility class for linking vulkan structs

---
+++ Source/Engine/VulkanRHI/VulkanUtils.h
@@ -0,0 +1,36 @@
+#pragma once
+
+
+namespace spt::vulkan
+{
+
+class VulkanStructsLinkedList
+{
+public:
+
+	VulkanStructsLinkedList()
+		: m_next(nullptr)
+	{ }
+
+	template<typename TStruct>
+	VulkanStructsLinkedList(TStruct& structure)
+		: m_next(&structure.pNext)
+	{ }
+
+	template<typename TStruct>
+	void Append(TStruct& structure)
+	{
+		if (m_next)
+		{
+			*m_next = &structure;
+		}
+
+		m_next = &structure.pNext;
+	}
+
+private:
+
+	const void** m_next;
+};
+
+}
