099b8d5ff82996661950487d95d03c93e3e630fe
Mateusz Kołodziejczyk <mateusz643@gmail.com>
2022-07-22 16:07:59 +0200

    -remove Debug directory from gitignore -move DebugMessenger.cpp to proper directory

---
+++ Source/Engine/VulkanRHI/Debug/DebugMessenger.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Vulkan.h"
+
+namespace spt::vulkan
+{
+
+class DebugMessenger
+{
+public:
+
+	static VkDebugUtilsMessengerCreateInfoEXT CreateDebugMessengerInfo();
+
+	static VkDebugUtilsMessengerEXT CreateDebugMessenger(VkInstance instance, const VkAllocationCallbacks* allocator);
+	static void DestroyDebugMessenger(VkDebugUtilsMessengerEXT messenger, VkInstance instance, const VkAllocationCallbacks* allocator);
+};
+
+}
