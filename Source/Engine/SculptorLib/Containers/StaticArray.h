83e0b7385cfa9ef53fff497afc8b17367ceff5ce
Mateusz Kołodziejczyk <mateusz643@gmail.com>
2022-08-13 17:19:51 +0200

    -change static array from class to alias

--- Source/Engine/SculptorLib/Containers/StaticArray.h
+++ Source/Engine/SculptorLib/Containers/StaticArray.h
@@ -7,16 +7,6 @@ namespace spt::lib
 {
 
 template <class Type, SizeType Size>
-class StaticArray : public std::array<Type, Size>
-{
-protected:
-
-	using Super = std::array<Type, Size>;
-
-public:
-
-	// All constructors from base class
-	using Super::Super;
-};
+using StaticArray = std::array<Type, Size>;
 
 }
