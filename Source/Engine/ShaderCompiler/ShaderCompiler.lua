30a1def28d391685f0c02bf74c950ad127232d85
Mateusz Kołodziejczyk <mateusz643@gmail.com>
2022-08-15 16:42:03 +0200

    -link spirv-cross to shader  compiler

--- Source/Engine/ShaderCompiler/ShaderCompiler.lua
+++ Source/Engine/ShaderCompiler/ShaderCompiler.lua
@@ -9,11 +9,30 @@ function ShaderCompiler:SetupConfiguration(configuration, platform)
     self:AddPrivateDependency("Tokenizer")
     self:AddPrivateDependency("Serialization")
 
+    self:AddLinkOption("-IGNORE:4099")
+
+    if configuration == EConfiguration.Debug
+    then
+        self:AddPrivateDependency("spirv-cross-cored")
+        self:AddPrivateDependency("spirv-cross-glsld")
+        -- debug libs from VulkanSDK doesn't have associated pdbs which causes link warning in debug configuration
+        self:AddLinkOption("-IGNORE:4099")
+    else
+        self:AddPrivateDependency("spirv-cross-cored")
+        self:AddPrivateDependency("spirv-cross-glsld")
+    end
+
+    self:AddPrivateAbsoluteIncludePath("$(Vulkan_SDK)/Include")
+
     self:SetupPlatformConfiguration(configuration, platform)
 end
 
 function ShaderCompiler:GetAdditionalLibPaths()
-    return self:GetPlatformAdditionalLibPaths()
+    return 
+    {
+        self:GetPlatformAdditionalLibPath(),
+        "$(VULKAN_SDK)/Lib" -- for spirv-cross
+    }
 end
 
 function ShaderCompiler:GetProjectFiles(configuration, platform)
--- Source/Engine/ShaderCompiler/Shaderc/Shaderc.lua
+++ Source/Engine/ShaderCompiler/Shaderc/Shaderc.lua
@@ -3,23 +3,15 @@ function ShaderCompiler:GetPlatformDirectoryName()
     return "Shaderc"
 end
 
-function ShaderCompiler:GetPlatformAdditionalLibPaths()
-    return
-    {
-        "$(VULKAN_SDK)/Lib"
-    }
+function ShaderCompiler:GetPlatformAdditionalLibPath()
+    return "$(VULKAN_SDK)/Lib"
 end
 
 function ShaderCompiler:SetupPlatformConfiguration(configuration, platform)
     if configuration == EConfiguration.Debug
     then
         self:AddPrivateDependency("shaderc_combinedd")
-        -- shaderc debug libs from VulkanSDK doesn't have associated pdbs which causes link warning in debug configuration
-        self:AddLinkOption("-IGNORE:4099")
     else
         self:AddPrivateDependency("shaderc_combined")
     end
-
-    self:AddPrivateAbsoluteIncludePath("$(Vulkan_SDK)/Include")
-
 end
