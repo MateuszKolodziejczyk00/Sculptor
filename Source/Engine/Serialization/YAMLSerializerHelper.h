af3173de180d03420020dc1d1228376f4e934ba5
Mateusz Kołodziejczyk <mateusz643@gmail.com>
2022-08-17 22:08:52 +0200

    -further work on YAML serialization helper

--- Source/Engine/Serialization/YAMLSerializerHelper.h
+++ Source/Engine/Serialization/YAMLSerializerHelper.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "SculptorCoreTypes.h"
-#include "DataSerializer.h"
+#include "YAML.h"
 
 
 namespace spt::srl
@@ -15,10 +15,21 @@ public:
 		: m_node(node)
 	{ }
 
+	constexpr Bool IsWriting() const
+	{
+		return true;
+	}
+
 	template<typename TDataType>
 	void Serialize(const TDataType& data) const
 	{
 		m_node.push_back(data);
+	}
+
+	template<typename TDataType>
+	void Serialize(lib::StringView key, const TDataType& data) const
+	{
+		m_node[key] = data;
 	}
 
 private:
@@ -36,10 +47,21 @@ public:
 		, m_currentIdx(0)
 	{ }
 
+	constexpr Bool IsWriting() const
+	{
+		return false;
+	}
+
 	template<typename TDataType>
 	void Serialize(TDataType& data)
 	{
 		data = m_node[m_currentIdx++].as<TDataType>();
+	}
+
+	template<typename TDataType>
+	void Serialize(const lib::String& key, TDataType& data)
+	{
+		data = m_node[key].as<TDataType>();
 	}
 
 private:
@@ -49,13 +71,137 @@ private:
 };
 
 
-template<typename TDataType>
-class YAMLSerializer
+class YAMLEmitter
 {
 public:
 
-	// Writer if type is const, otherwise loader
-	using Type = std::conditional_t<std::is_const_v<TDataType>, YAMLWriter, YAMLLoader>;
+	YAMLEmitter(YAML::Emitter& emitter)
+		: m_emitter(emitter)
+		, m_emittingSequence(false)
+	{ }
+
+	constexpr Bool IsWriting() const
+	{
+		return false;
+	}
+
+	template<typename TDataType>
+	void Serialize(const TDataType& data) const
+	{
+		if (!m_emittingSequence)
+		{
+			m_emitter << YAML::BeginSeq;
+			m_emittingSequence = true;
+		}
+
+		m_emitter << data;
+	}
+
+	template<typename TDataType>
+	void Serialize(lib::StringView key, const TDataType& data) const
+	{
+		if (m_emittingSequence)
+		{
+			m_emitter << YAML::EndSeq;
+			m_emittingSequence = false;
+		}
+
+		m_emitter << YAML::Key << key << YAML::Value << data;
+	}
+
+	void EndEmitting() const
+	{
+		if (m_emittingSequence)
+		{
+			m_emitter << YAML::EndSeq;
+		}
+	}
+
+private:
+
+	YAML::Emitter&	m_emitter;
+	Bool			m_emittingSequence;
 };
 
+
+class YAMLSerializeHelper
+{
+public:
+
+	template<typename Type>
+	static YAML::Node Write(const Type& data)
+	{
+		YAML::Node node;
+		YAMLWriter serializer(node);
+		TypeSerializer<Type>::Serialize(serializer, data);
+		return node;
+	}
+
+	template<typename Type>
+	static bool Load(const YAML::Node& node, Type& data)
+	{
+		YAMLLoader serializer(node);
+		TypeSerializer<Type>::Serialize(serializer, data);
+		return true;
+	}
+
+	template<typename Type>
+	static void Emit(YAML::Emitter& emitter, const Type& data)
+	{
+		YAMLEmitter serializer(emitter);
+		TypeSerializer<Type>::Serialize(serializer, data);
+		serializer.EndEmitting();
+	}
+};
+
+
+template<typename Type>
+struct TypeSerializer
+{
+	// Param is always Type, but may be const or not
+	template<typename Serializer, typename Param>
+	static void Serialize(Serializer& serializer, Param& data)
+	{
+		// ...
+	}
+};
+
+
+template<typename SerializedType, typename Serializer, typename Param>
+void Serialize(Serializer& serializer, Param& data)
+{
+	if constexpr (std::is_const_v<Param>)
+	{
+		TypeSerializer<const SerializedType>::Serialize(serializer, data);
+	}
+	else
+	{
+		TypeSerializer<std::remove_const_t<SerializedType>>::Serialize(serializer, data);
+	}
 }
+
+} // spt::srl
+
+#define SPT_YAML_SERIALIZATION_TEMPLATES(type)										\
+namespace YAML																		\
+{																					\
+inline Emitter& operator<<(Emitter& emitter, const type& data)						\
+{																					\
+	spt::srl::YAMLSerializeHelper::Emit(emitter, data);								\
+	return emitter;																	\
+}																					\
+																					\
+template<>																			\
+struct convert<type>																\
+{																					\
+	static Node encode(const type& data)											\
+	{																				\
+		return spt::srl::YAMLSerializeHelper::Write(data);							\
+	}																				\
+																					\
+	static bool decode(const Node& node, type& data)								\
+	{																				\
+		return spt::srl::YAMLSerializeHelper::Load(node, data);						\
+	}																				\
+};																					\
+} 																					\
