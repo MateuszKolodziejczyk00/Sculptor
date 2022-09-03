5e5814f741fa1aba8556b156505f68855fb596ab
Mateusz Kołodziejczyk <mateusz643@gmail.com>
2022-09-03 23:14:44 +0200

    -implement initialization of renderer compute pipeline

--- Source/Engine/RendererTypes/Types/Pipeline/ComputePipeline.cpp
+++ Source/Engine/RendererTypes/Types/Pipeline/ComputePipeline.cpp
@@ -1,4 +1,5 @@
 #include "ComputePipeline.h"
+#include "Types/Shader.h"
 
 namespace spt::rdr
 {
@@ -8,7 +9,16 @@ ComputePipeline::ComputePipeline(const RendererResourceName& name, const lib::Sh
 {
 	SPT_PROFILE_FUNCTION();
 
-	SPT_CHECK_NO_ENTRY();
+	const lib::DynamicArray<rhi::RHIShaderModule>& shaderModules = shader->GetShaderModules();
+	SPT_CHECK(shaderModules.size() == 1); //  compute shader must have only one module (compute)
+
+	const rhi::RHIShaderModule& computeShaderModule = shaderModules[0];
+	SPT_CHECK(computeShaderModule.GetStage() == rhi::EShaderStage::Compute);
+
+	const rhi::PipelineLayoutDefinition pipelineLayoutDef = CreateLayoutDefinition(*GetMetaData());
+
+	GetRHI().InitializeRHI(computeShaderModule, pipelineLayoutDef);
+	GetRHI().SetName(name.Get());
 }
 
 } // spt::renderer
