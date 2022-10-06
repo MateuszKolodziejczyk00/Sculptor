2db1b68f226bdc7f606271a68f41b497aec72177
Mateusz Kołodziejczyk <mateusz643@gmail.com>
2022-10-06 23:04:21 +0200

    -make regexes static to minimize number of times when we have to construct them

--- Source/Engine/ShaderCompiler/Common/Preprocessor/ShaderFilePreprocessor.cpp
+++ Source/Engine/ShaderCompiler/Common/Preprocessor/ShaderFilePreprocessor.cpp
@@ -50,8 +50,8 @@ ShaderFilePreprocessingResult ShaderFilePreprocessor::PreprocessShaderFileSource
 
 	ShaderFilePreprocessingResult result;
 	
-	const std::regex shaderTypeRegex(R"~(#type\(\s*\w+\s*\))~");
-	const std::regex shaderTypeNameRegex(R"~(\w+(?=\s*\)))~");
+	static const std::regex shaderTypeRegex(R"~(#type\(\s*\w+\s*\))~");
+	static const std::regex shaderTypeNameRegex(R"~(\w+(?=\s*\)))~");
 
 	auto shadersBeginMacros = std::sregex_iterator(std::cbegin(sourceCode), std::cend(sourceCode), shaderTypeRegex);
 
