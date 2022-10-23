f65513ed22063cbdbc0d675c93c88843737e8879
Mateusz Kołodziejczyk <mateusz643@gmail.com>
2022-10-23 12:07:51 +0200

    -utility types

---
+++ Source/Engine/SculptorLib/Utility/Templates/TypeStorage.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "MathTypes.h"
+
+
+namespace spt::lib
+{
+
+template<typename TType>
+struct TypeStorage
+{
+public:
+
+	TypeStorage() = default;
+
+	template<typename... TArgs>
+	TypeStorage(TArgs&&... args)
+	{
+		Construct(std::forward<TArgs>(args)...);
+	}
+
+	template<typename... TArgs>
+	void Construct(TArgs&&... args)
+	{
+		new (m_storage) TType(std::forward<TArgs>(args)...);
+	}
+
+	void Destroy()
+	{
+		Get().~TType();
+	}
+
+	TType& Get()
+	{
+		return *reinterpret_cast<TType*>(m_storage);
+	}
+	
+	const TType& Get() const
+	{
+		return *reinterpret_cast<const TType*>(m_storage);
+	}
+
+private:
+
+	alignas(alignof(TType)) Byte m_storage[sizeof(TType)];
+};
+
+} // spt::lib
---
+++ Source/Engine/SculptorLib/Utility/Threading/ThreadUtils.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <new>
+
+
+namespace spt::lib
+{
+
+struct InterferenceProps
+{
+	// Minimum offset between two objects to avoid false sharing
+	static constexpr SizeType destructiveInterferenceSize	= std::hardware_destructive_interference_size;
+
+	// Maximum size of contiguous memory to promote true sharing.
+	static constexpr SizeType constructiveInterferenceSize	= std::hardware_constructive_interference_size;
+};
+
+} // spt::lib
