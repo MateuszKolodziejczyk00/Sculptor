c954c3fbbe5118381aecea5dbccd1e2ea0ac4de9
Mateusz Kołodziejczyk <mateusz643@gmail.com>
2023-03-10 18:38:56 +0100

    -refactor array of srv textures binding to make sure that we won't overwrite textures that may be still used in running shaders

--- Source/Engine/Graphics/DescriptorSetBindings/ArrayOfSRVTexturesBinding.h
+++ Source/Engine/Graphics/DescriptorSetBindings/ArrayOfSRVTexturesBinding.h
@@ -5,6 +5,8 @@
 #include "TextureBindingTypes.h"
 #include "ShaderStructs/ShaderStructsTypes.h"
 #include "Utility/String/StringUtils.h"
+#include "Renderer.h"
+#include "RendererSettings.h"
 
 namespace spt::gfx
 {
@@ -23,7 +25,7 @@ public:
 	explicit ArrayOfSRVTexturesBinding(const lib::HashedString& name)
 		: Super(name)
 	{
-		std::generate_n(std::back_inserter(m_availableArrayIndices), arraySize, [i = static_cast<Uint32>(arraySize)]() mutable { return --i; });
+		std::generate_n(std::back_inserter(m_availableArrayIndices), arraySize, [ i = static_cast<Uint32>(arraySize) ]() mutable { return AvailableIndex{ --i, idxNone<Uint32> }; });
 	}
 
 	virtual void UpdateDescriptors(rdr::DescriptorSetUpdateContext& context) const final
@@ -65,8 +67,19 @@ public:
 	{
 		SPT_CHECK(!m_availableArrayIndices.empty());
 
-		const Uint32 arrayIndex = m_availableArrayIndices.back();
-		m_availableArrayIndices.pop_back();
+		const Uint64 currentFrameIdx	= rdr::Renderer::GetCurrentFrameIdx();
+		const Uint64 framesInFlight		= rdr::RendererSettings::Get().framesInFlight;
+
+		const auto foundAvailableIdxIt = std::find_if(std::crbegin(m_availableArrayIndices), std::crend(m_availableArrayIndices),
+													  [ = ](const AvailableIndex& availableIdx)
+													  {
+														  return availableIdx.lastUseFrameIdx == idxNone<Uint32> || currentFrameIdx >= availableIdx.lastUseFrameIdx + framesInFlight;
+													  });
+
+		SPT_CHECK(foundAvailableIdxIt != std::crend(m_availableArrayIndices));
+	
+		const Uint32 arrayIndex = foundAvailableIdxIt->arrayIndex;
+		m_availableArrayIndices.erase(std::next(foundAvailableIdxIt).base());
 		m_boundTextures.emplace_back(BoundTexture{ textureView, arrayIndex });
 
 		MarkAsDirty();
@@ -78,7 +91,8 @@ public:
 	{
 		SPT_CHECK(textureArrayIdx != idxNone<Uint32>);
 		SPT_CHECK(std::find(std::cbegin(m_availableArrayIndices), std::cend(m_availableArrayIndices), textureArrayIdx) == std::cend(m_availableArrayIndices));
-		m_availableArrayIndices.emplace(textureArrayIdx);
+		
+		m_availableArrayIndices.emplace_back(AvailableIndex{ textureArrayIdx, rdr::Renderer::GetCurrentFrameIdx() });
 
 		const auto boundTextureIt = std::find_if(std::cbegin(m_boundTextures), std::cend(m_boundTextures),
 												 [textureArrayIdx](const BoundTexture& boundTexture)
@@ -100,8 +114,14 @@ private:
 		Uint32 arrayIndex;
 	};
 
-	lib::DynamicArray<Uint32>		m_availableArrayIndices;
-	lib::DynamicArray<BoundTexture>	m_boundTextures;
+	struct AvailableIndex
+	{
+		Uint32 arrayIndex;
+		Uint64 lastUseFrameIdx;
+	};
+
+	lib::DynamicArray<AvailableIndex>	m_availableArrayIndices;
+	lib::DynamicArray<BoundTexture>		m_boundTextures;
 };
 
 
