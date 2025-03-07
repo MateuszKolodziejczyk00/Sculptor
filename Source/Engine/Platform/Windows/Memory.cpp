359047d27227b1742463cd7d40b20465ce2b5773
Mateusz Kołodziejczyk <mateusz643@gmail.com>
2025-03-07 20:46:20 +0100

    -remove temp stuff -add less temp stuff

---
+++ Source/Engine/Platform/Windows/Memory.cpp
@@ -0,0 +1,33 @@
+#include "Memory.h"
+#include "Windows.h"
+
+
+namespace spt::mem
+{
+
+SizeType GetPageSize()
+{
+	return 4096u;
+}
+
+Byte* ReserveVirtualMemory(SizeType size)
+{
+	return reinterpret_cast<Byte*>(VirtualAlloc(nullptr, size, MEM_RESERVE, PAGE_READWRITE));
+}
+
+void ReleaseVirtualMemory(Byte* address, SizeType size)
+{
+	VirtualFree(address, 0, MEM_RELEASE);
+}
+
+void CommitVirtualMemory(Byte* address, SizeType size)
+{
+	VirtualAlloc(address, size, MEM_COMMIT, PAGE_READWRITE);
+}
+
+void DecommitVirtualMemory(Byte* address, SizeType size)
+{
+	VirtualFree(address, size, MEM_DECOMMIT);
+}
+
+} // spt::mem
